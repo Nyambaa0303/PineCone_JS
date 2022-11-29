2626918805582bc1e65e126018087234735566ac
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-11-29 13:22:56 +0800

    update

---
+++ 11_sar_28/home_exercise/main.js
@@ -0,0 +1,68 @@
+// gurvaljingiin talbai
+var p = 0
+var a = 5
+var b = 3 
+var c = 7
+
+p = a + b + c
+
+console.log(p)
+
+// toirgiin talbai
+
+
+var p, s, r;
+const PTI=3.14;
+ s = 0;
+ r = 5;
+
+s = (p*r)**2
+
+console.log(s)
+
+// 15 megabyte data 3 seconded manai onlainaar tsatsaj chaddag bol tuunii bit rate-g olno uu\\
+
+/**
+ * 1mb/s = 1000000 bit/s*/
+
+ let mb, sec, bRate
+
+ mb = 1000000
+ sec = 3
+
+
+ bRate = (1000000 * 15)/3
+
+ console.log(bRate)
+
+
+//  ogson toog tselseees farangeit ruu horvuul
+
+let farangeit, cels
+cels = 30
+
+farangeit = 9 / 5 * cels +32
+
+console.log(farangeit)
+
+//  ogson toog farangeitees cels ruu horuul
+
+let far, cel
+far = -30
+
+cel = 5 / 9 * (far - 32)
+
+
+// ogogdson 2 too zarlaad math-n 5-n uildel d horvuul
+
+let a1, b1, c2, d2, e2, f2, g2
+
+a1 = 15
+b1 = 10
+d2 = a1 + b1
+c2 = a1 - b1 
+e2 = a1 * b1
+f2 = a1 / b1 
+g2 = a1 % b1
+
+console.log( d2, c2, e2, f2 , g2)
---
+++ 11_sar_28/home_exercise/variable.js
@@ -0,0 +1,169 @@
+
+// m=y2-y1/x2-x1
+
+let m = 0;
+let y1 = -6;
+let x1 = -6;
+let y2 = 8;
+let x2 = 8;
+m = (y2 - y1) / (x2 - x1);
+
+
+console.log(m);
+
+
+// 15 gegabity heden bit ve
+
+var gb, meb ,kb, byte, bit 
+gb = 1024
+meb = 1024 
+kb = 1024
+byte = 8
+
+bit = gb * meb * kb * byte
+
+bit = 15 * bit
+
+console.log(bit)
+
+
+// gurvaljingiin talbai oloh
+
+let side1, side2, side3, side
+
+side1 = 5
+side2 = 4
+side3 = 7
+
+side = side1 + side2 + side3
+
+console.log(side)
+
+
+// sm-g inch ruu horvuuleh inch-g sm ruu horvuuleh
+
+let centimeter, inch
+
+inch = 20
+centimeter = 65
+
+centimeter = inch * 2.54
+inch = centimeter / 2.54
+
+console.log( centimeter)
+
+// garaas duguin radius buyu r oruulahad duguin ezelhuuniig ol garaas r oruulahad toirgiin urtiig ol
+
+let radius, talbai, t_urt,PTI_p
+PTI_p = 3.14
+radius = 5
+
+t_urt = 2*radius * PTI_p
+talbai = PTI_p * radius**2
+
+console.log(t_urt, talbai)
+
+
+//  3-n orontoi toonii hamgiin ehnii tsipriig  oloh ilerhiilel bich
+
+let aToo, bToo, cToo, dToo
+aToo = 564
+bToo = aToo % 100
+cToo = aToo - bToo
+dToo = cToo / 100
+
+
+console.log(dToo)
+
+
+// baby shark duuniii ugiig hamgiin ihdee 12 huvsagch ashiglan doosh n tsuvruulj hiine uu
+
+let baby, mommy, dady, grandma, grandpa, letsGo, runAway, safe, it, doo
+
+doo = ", doo-doo, doo-doo"
+baby = 'Baby Shark'
+mommy = 'Mommy Shark'
+dady = 'Daddy Shark'
+grandma = 'Grandma Shark'
+grandpa = 'Grandpa Shark'
+letsGo = "Let's go hunt"
+runAway = "Run away"
+safe = "Safe at last "
+it = "It's the end"
+
+
+console.log(baby + doo)
+console.log(baby + doo)
+console.log(baby + doo)
+console.log(baby)
+
+console.log( " ")
+
+console.log(mommy + doo)
+console.log(mommy + doo)
+console.log(mommy + doo)
+console.log(mommy)
+
+
+console.log( " ")
+
+
+console.log(dady + doo)
+console.log(dady + doo)
+console.log(dady + doo)
+console.log(dady)
+
+
+console.log( " ")
+
+
+console.log(grandma + doo)
+console.log(grandma + doo)
+console.log(grandma + doo)
+console.log(grandma)
+
+
+console.log( " ")
+
+
+console.log(grandpa + doo)
+console.log(grandpa + doo)
+console.log(grandpa + doo)
+console.log(grandpa)
+
+
+console.log( " ")
+
+
+console.log(letsGo + doo)
+console.log(letsGo + doo)
+console.log(letsGo + doo)
+console.log(letsGo)
+
+
+console.log( " ")
+
+
+console.log(runAway + doo)
+console.log(runAway + doo)
+console.log(runAway + doo)
+console.log(runAway + '(Ah!)')
+
+
+console.log( " ")
+
+
+console.log(safe + doo)
+console.log(safe + doo)
+console.log(safe + doo)
+console.log(safe + '(phew)')
+
+
+console.log( " ")
+
+
+console.log(it + doo)
+console.log(it + doo)
+console.log(it + doo)
+console.log(it)
+
