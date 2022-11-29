91afc74a4af1d675b442a425ff69870077f3d481
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-11-29 18:50:12 +0800

    u

---
+++ 11_sar_29/home_condition/variable/main.js
@@ -0,0 +1,47 @@
+let a = 89
+let b = 45
+let c = 98
+let d = 34
+
+let niilber = 0 ;
+
+if(a > 80){
+    niilber = niilber + a
+}
+if(b > 80){
+    niilber = niilber + b
+}
+if(c > 80){
+    niilber = niilber + c
+}
+if(d > 80){
+    niilber = niilber + d
+}
+
+console.log('80-s ih toonii niilber =' + niilber)
+
+
+//  4-n too og ogson toonii 5-s baga toonii urjveriig ol
+
+
+let e = 4
+let f = 2 
+let h = 7 
+let g = 3
+
+let sum = 1
+
+if (e < 5){
+    sum = sum * e
+}
+if (f < 5){
+    sum = sum * f
+}
+if (h < 5){
+    sum = sum * h
+}
+if(g < 5){
+    sum = sum * g
+}
+
+console.log('vrjwer = ' + sum) 
