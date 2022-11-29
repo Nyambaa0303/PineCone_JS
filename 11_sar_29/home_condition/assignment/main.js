d006702a2c18af54d5fb4f49fedd10276a11283b
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-11-29 20:03:07 +0800

    u

---
+++ 11_sar_29/home_condition/assignment/main.js
@@ -0,0 +1,96 @@
+let a1 = 4 > 3 // true
+let a2 = 4 < 3 // false
+let a3 = 4 >= 3 // true
+let a4 = 4 <= 3 // false
+let a5 = 4 == 4 // true
+let a6 = 4 === 4 // true
+let a8 = 4 != 4 // false
+let a9 = 4 !== 4 // false
+let a10 = 4 != "4" // false
+let a11= 4 == "4" // true
+let a12 = 4 === "4" //false
+
+
+console.log(a1)
+
+console.log(a2)
+
+console.log(a3)
+
+console.log(a4)
+
+console.log(a5)
+
+console.log(a6)
+
+console.log(a8)
+
+console.log(a9)
+
+console.log(a10)
+
+console.log(a11)
+
+console.log(a12)
+
+console.log(" ")
+
+console.log('number2')
+
+
+console.log(" ")    
+// number 2
+
+let one = 4 > 3 && 10 < 12;   //true
+let two = 4 > 3 && 10 > 12;   // false
+let three = 4 > 3 || 10 < 12;  // true
+let four = 4 > 3 || 10 > 12;  // true
+let five = !(4 > 3);  // false
+let six = !(4 < 3);  // true
+let eight = !(4 > 3 && 10 < 12);  // false
+let nine = !(4 > 3 && 10 > 12);  // true
+let ten = !(4 === "4");  // true
+
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four)
+console.log(five)
+console.log(six)
+console.log(eight)
+console.log(nine)
+console.log(ten)
+
+
+// number3
+
+
+console.log(" ")
+
+console.log('number3')
+
+
+console.log(" ")
+
+
+let yourAge = prompt('Nasaa oruulna uu?')
+let myAge = 31
+
+if(yourAge > myAge){
+    console.log("Ta nadaas ah ym baina.")
+}else{
+    console.log('Ta nadaas duu ym baina.')
+}
+
+
+
+console.log(" ")
+
+console.log('number4')
+
+
+console.log(" ")
+
+// number4
+
+
