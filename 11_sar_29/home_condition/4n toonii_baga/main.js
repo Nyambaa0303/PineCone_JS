652fb6d040ebe92ce7481e56bcd2b46ae626a78c
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-11-30 12:13:07 +0800

    update

---
+++ 11_sar_29/home_condition/4n toonii_baga/main.js
@@ -0,0 +1,19 @@
+// 4 n toonii hamgiin baga toog ol
+
+let a = prompt("a toog oruulna uu")
+let b = prompt("b toog oruulna uu")
+let c = prompt("c toog oruulna uu")
+let d = prompt("d toogoo oruulna uu")
+
+
+if(a < b){
+    console.log('A too n hamgiin baga too :',a)
+}else if(b < c){
+    console.log('B too n hamgiin baga too n b-too:',b)
+}else if(c < d){
+    console.log('C too n hamgiin baga too n c-too:', c)
+}else{
+    console.log('D too n hamgiin baga too n d-too:', d )
+}
+
+
