db4ff5eedc3c3c64d9252e80606f4e6c6f97b688
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-11-30 12:22:05 +0800

    u

--- 11_sar_29/home_condition/4n toonii_baga/main.js
+++ 11_sar_29/home_condition/4n toonii_baga/main.js
@@ -2,18 +2,18 @@
 
 let a = prompt("a toog oruulna uu")
 let b = prompt("b toog oruulna uu")
-let c = prompt("c toog oruulna uu")
+let c = prompt("c toog oruulna uu")   
 let d = prompt("d toogoo oruulna uu")
 
 
-if(a < b){
-    console.log('A too n hamgiin baga too :',a)
-}else if(b < c){
-    console.log('B too n hamgiin baga too n b-too:',b)
-}else if(c < d){
-    console.log('C too n hamgiin baga too n c-too:', c)
+if(a < b && a < c && a < d){
+    console.log('A too n hamgiin baga baina:',a)
+}else if(b < a && b < c && b < d){
+    console.log('B too n hamgiin baga baina:',b)
+}else if(c < a && c < b && c < d){
+    console.log('C too n hamgiin baga baina:', c)
 }else{
-    console.log('D too n hamgiin baga too n d-too:', d )
+    console.log('D too n hamgiin baga baina:', d )
 }
 
 
