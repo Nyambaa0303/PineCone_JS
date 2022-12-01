7e99812182450c96cdf9534a3cc5ec718ffcde49
Evening <evening@admins-iMac.local>
2022-12-01 19:36:07 +0800

    update

--- 11_sar_29/home_condition/variable/main.js
+++ 11_sar_29/home_condition/variable/main.js
@@ -1,9 +1,9 @@
 let a = 89;
-let b = 45;
+let b = 55;
 let c = 98;
 let d = 34;
 
-let niilber;
+let niilber = 0;
 
 if (a > 80) {
   niilber = niilber + a;
@@ -18,28 +18,28 @@ if (d > 80) {
   niilber = niilber + d;
 }
 
-console.log("80-s ih toonii niilber =" + niilber);
+console.log("80-s ih toonii niilber = ", niilber);
 
 //  4-n too og ogson toonii 5-s baga toonii urjveriig ol
 
-let e = 4;
-let f = 2;
-let h = 7;
-let g = 3;
+// let e = 4;
+// let f = 2;
+// let h = 7;
+// let g = 3;
 
-let sum = 1;
+// let sum = 1;
 
-if (e < 5) {
-  sum = sum * e;
-}
-if (f < 5) {
-  sum = sum * f;
-}
-if (h < 5) {
-  sum = sum * h;
-}
-if (g < 5) {
-  sum = sum * g;
-}
+// if (e < 5) {
+//   sum = sum * e;
+// }
+// if (f < 5) {
+//   sum = sum * f;
+// }
+// if (h < 5) {
+//   sum = sum * h;
+// }
+// if (g < 5) {
+//   sum = sum * g;
+// }
 
-console.log("vrjwer = " + sum);
+// console.log("vrjwer = " + sum);
---
+++ 12_sar_01/home_exercises/Tegsh_toog_oloh/main.js
@@ -0,0 +1,5 @@
+let n = 0;
+while (n < 100) {
+  console.log(n);
+  n = n + 2;
+}
