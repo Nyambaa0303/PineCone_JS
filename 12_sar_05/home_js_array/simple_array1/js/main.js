ab37d48ab91ca2b397159f3d3843509d4f721371
Evening <evening@admins-iMac.local>
2022-12-06 13:29:42 +0800

    u

--- 12_sar_05/home_js_array/simple_array1/js/main.js
+++ 12_sar_05/home_js_array/simple_array1/js/main.js
@@ -1,17 +1,29 @@
-let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+let itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
 
 console.log(itCompanies);
 
-console.log('\n')
+console.log("\n");
 // '=====================array dahi companii Length hevle======================
-console.log('=====================array dahi companii Ld hevle======================')
-console.log('\n')
-let count = 0
-for(let i = 0; i <= itCompanies.length; i++){
- console.log(itCompanies[i], itCompanies[i].length);
+console.log(
+  "=====================array dahi companii Ld hevle======================"
+);
+console.log("\n");
+let count = 0;
+for (let i = 0; i <= itCompanies.length; i++) {
+  console.log(itCompanies[i], itCompanies[i].length);
 }
 
-console.log('\n')
+console.log("\n");
 // '=====================ehnii compani, dund boon suulchiin companiig hevle======================
-console.log('=====================array dahi companii Legth hevle======================')
-console.log('\n')
+console.log(
+  "=====================array dahi companii Legth hevle======================"
+);
+console.log("\n");
