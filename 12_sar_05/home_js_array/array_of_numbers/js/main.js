7223acc40eafa44fa1291d9795b3540dc4e354b6
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-08 09:07:35 +0800

    update

--- 12_sar_05/home_js_array/array_of_numbers/js/main.js
+++ 12_sar_05/home_js_array/array_of_numbers/js/main.js
@@ -1,10 +1,12 @@
 let arrayOfNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
 
+console.log(arrayOfNumbers)
+
 console.log("\n");
 // '==========================================================================
 console.log("==========Array dotorhi toonuudiin niilberiig ol=======");
 let sum = 0;
-for (i = 0; i < arrayOfNumbers.length; i++) {
+for (let i = 0; i < arrayOfNumbers.length; i++) {
   sum = sum + arrayOfNumbers[i];
 }
 console.log(sum);
@@ -13,8 +15,31 @@ console.log("\n");
 // '==========================================================================
 console.log("==========Array dotorhi hamgiin ih toog ol=======");
 let max = 0;
-for (i = 0; i < arrayOfNumbers.length; i++) {
-  if ((max = max < arrayOfNumbers.length[i])) {
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  if (arrayOfNumbers[i] > max) {
+    max = arrayOfNumbers[i];
   }
-  console.log(max);
 }
+console.log(max);
+
+console.log("\n");
+// '==========================================================================
+console.log("==========Array dotorhi hamgiin baga toog ol=======");
+let min;
+
+
+for(let i = 0; i < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] > min){
+    }else{
+        min = arrayOfNumbers[i];
+    }
+}
+console.log(min);
+
+console.log("\n");
+// '==========================================================================
+console.log("========== Array ehend duriin neg toog nem =======");
+
+console.log(arrayOfNumbers.unshift(12));
+console.log(arrayOfNumbers.push(45));
+console.log(arrayOfNumbers)
