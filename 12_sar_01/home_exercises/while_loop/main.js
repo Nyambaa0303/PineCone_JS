33d4184fc86d6d9c3cd55b03732fa48338ffc4cf
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-09 13:54:09 +0800

    update

--- 12_sar_01/home_exercises/while_loop/main.js
+++ 12_sar_01/home_exercises/while_loop/main.js
@@ -1,83 +1,83 @@
-// //  10 hurtelh natural too
+//  10 hurtelh natural too
 
-// let count = 1;
+let count = 1;
 
-// while (count <= 10) {
-//   console.log(count);
-//   count++;
-// }
+while (count <= 10) {
+  console.log(count);
+  count++;
+}
 
-// //  10 hurtelh natural toonii sondgoi toog ol
+//  10 hurtelh natural toonii sondgoi toog ol
 
-// let num = 1;
+let num = 1;
 
-// while (num < 10) {
-//   console.log(num);
-//   num += 2;
-// }
+while (num < 10) {
+  console.log(num);
+  num += 2;
+}
 
-// // 10 hvrtelh natural toonii tegshiig ol
+// 10 hvrtelh natural toonii tegshiig ol
 
-// let j = 2;
+let j = 2;
 
-// while (j < 10) {
-//   console.log(j);
-//   j += 2;
-// }
-// // 100 hurtelh natural toonii niilberiig oloh programm bich
+while (j < 10) {
+  console.log(j);
+  j += 2;
+}
+// 100 hurtelh natural toonii niilberiig oloh programm bich
 
-// const l = 100;
-// let n = 0;
+const l = 100;
+let n = 0;
 
-// let m = 0;
+let m = 0;
 
-// while (n < l) {
-//   n++;
-//   m = m + n;
-// }
-// console.log(m);
+while (n < l) {
+  n++;
+  m = m + n;
+}
+console.log(m);
 
-// //N hurtelh natural toog hevleh bolon tedgeer toonii niilberiig ol
+//N hurtelh natural toog hevleh bolon tedgeer toonii niilberiig ol
 
-// let x = Number(prompt("N toog oruulna uu?"));
+let x = Number(prompt("N toog oruulna uu?"));
 
-// y = 0;
-// z = 0;
+y = 0;
+z = 0;
 
-// while (y < x) {
-//   console.log(y);
-//   y++;
+while (y < x) {
+  console.log(y);
+  y++;
 
-//   z = z + y;
-// }
-// console.log(z);
+  z = z + y;
+}
+console.log(z);
 
-// // Ogogdson toog anhnii mon esehiig oloh kod bich
+// Ogogdson toog anhnii mon esehiig oloh kod bich
 
-// let s = Number(prompt("Toogoo oruulna uu?"));
+let s = Number(prompt("Toogoo oruulna uu?"));
 
-// let ok = true;
+let ok = true;
 
-// if (s === 2) {
-//   console.log("Anhnii too mon");
-// } else {
-//   let i = 2;
-//   while (i < s) {
-//     if (s % i === 0) {
-//       ok = false;
-//       break;
-//     }
-//     i++;
-//   }
-// }
-// if (ok) {
-//   console.log("Anhnii too mon");
-// } else {
-//   console.log("Anhnii too bish");
+if (s === 2) {
+  console.log("Anhnii too mon");
+} else {
+  let i = 2;
+  while (i < s) {
+    if (s % i === 0) {
+      ok = false;
+      break;
+    }
+    i++;
+  }
+}
+if (ok) {
+  console.log("Anhnii too mon");
+} else {
+  console.log("Anhnii too bish");
 
-// }
+}
 
-// // Ogogdson interval dahi anhnii toog oloh programm bichne uu
+// Ogogdson interval dahi anhnii toog oloh programm bichne uu
 
 let pNum1 = Number(prompt("Toogoo oruulna uu"));
 let pNum2 = Number(prompt("Toogoo oruulna uu"));
@@ -96,54 +96,74 @@ for (let i = pNum1; i <= pNum2; i++) {
   primeNumber = 0;
 }
 
-// // N toonii factorial oloh programm bich
+// N toonii factorial oloh programm bich
 
-// let facNum = Number(prompt("Toogoo oruulna uu?"));
-// let i = 1;
-// let multiple = 1;
+let facNum = Number(prompt("Toogoo oruulna uu?"));
+let i = 1;
+let multiple = 1;
 
-// while (i <= facNum) {
-//   multiple = multiple * i;
-//   i++;
-// }
+while (i <= facNum) {
+  multiple = multiple * i;
+  i++;
+}
 
-// console.log(multiple);
+console.log(multiple);
 
-// // N too oruulahad tsipruudiin niilberiig ol
+// N too oruulahad tsipruudiin niilberiig ol
 
-// let numb = Number(prompt('Toogoo oruulna uu?'));
-// let sum = 0;
+let numb = Number(prompt('Toogoo oruulna uu?'));
+let sum = 0;
 
-// for(let i = 0; i < numb.length; i++){
+for(let i = 0; i < numb.length; i++){
 
-//   sum = sum + numb[i];
+  sum = sum + numb[i];
 
-// }
-// console.log(sum)
+}
+console.log(sum)
 
-// // N toonii sondgoi too hevleh bolon sondgoi toonii niilberiig ol
+// N toonii sondgoi too hevleh bolon sondgoi toonii niilberiig ol
 
-// let Numb = Number(prompt('Toogoo oruulna uu'))
+let Numb = Number(prompt('Toogoo oruulna uu'))
 
-// let oddSum = 0;
+let oddSum = 0;
 
-// for (i = 1; i <= Numb; i++){
-//     if(i % 2 != 0){
-//       console.log(i)
-//       oddSum = oddSum + i;
-//     }
-// }
-// console.log(oddSum)
+for (i = 1; i <= Numb; i++){
+    if(i % 2 != 0){
+      console.log(i)
+      oddSum = oddSum + i;
+    }
+}
+console.log(oddSum)
 
-// //  N toonii tegsh tooog olj hevleh tedgeeriin niilberiig oloh programm bich
+//  N toonii tegsh tooog olj hevleh tedgeeriin niilberiig oloh programm bich
 
-// let even = Number(prompt('toogoo oruulna uu'));
-// let evenSum = 0;
+let even = Number(prompt('toogoo oruulna uu'));
+let evenSum = 0;
 
-// for(i = 1; i <= even; i++){
-//   if(i % 2 == 0){
-//     console.log(i);
-//     evenSum = evenSum + i;
-//   }
-// }
-// console.log(evenSum)
+for(i = 1; i <= even; i++){
+  if(i % 2 == 0){
+    console.log(i);
+    evenSum = evenSum + i;
+  }
+}
+console.log(evenSum)
+
+console.log("\n");
+console.log("----- 13. Өгөдсөн тооны цифрүүдийг үгээр хэвлэх программ бич-------");
+
+let urvuu = prompt('Toogoo oruulna uu?');
+
+
+for(let i = 0; i <= urvuu.length; i++){
+  if(urvuu[i] == 0) console.log(urvuu[i] + '=' + 'teg');
+  if(urvuu[i] == 1) console.log(urvuu[i] + '=' + 'neg');
+  if(urvuu[i] == 2) console.log(urvuu[i] + '=' + 'hoyr');
+  if(urvuu[i] == 3) console.log(urvuu[i] + '=' + 'gurav');
+  if(urvuu[i] == 4) console.log(urvuu[i] + '=' + 'dorov');
+  if(urvuu[i] == 5) console.log(urvuu[i] + '=' + 'tav');
+  if(urvuu[i] == 6) console.log(urvuu[i] + '=' + 'zurgaa');
+  if(urvuu[i] == 7) console.log(urvuu[i] + '=' + 'doloo');
+  if(urvuu[i] == 8) console.log(urvuu[i] + '=' + 'naim');
+  if(urvuu[i] == 9) console.log(urvuu[i] + '=' + 'ys');
+
+}
--- 12_sar_05/home_js_array/Davhar_utga/js/main.js
+++ 12_sar_05/home_js_array/Davhar_utga/js/main.js
@@ -1,8 +1,21 @@
 let array = [4, 2, 34, 4, 1, 12, 1, 4];
 let davhar = [];
-
-for (let i = 0; i < array.length; i++){
-    for(array[j] = 0; array[j] < array.length; array[j]++){
-        if(i )
+let d_i = 0;
+//debugger
+for (let i = 0; i < array.length - 1; i++){
+    for(let j = i + 1; j < array.length; j++ ){
+            if(array[i] == array[j])
+            {
+                davhar[davhar.length] = array[i];
+            }
+        }
+    for(let k = davhar.length - 2; k >= 0; k--)
+    {
+        if(davhar[davhar.length - 1] == davhar[k])
+        {
+            davhar.length--;
+        }
     }
+    
 }
+console.log(davhar);
