93d708e8f3a048995912e4e8a8515c442f2f167b
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-13 19:46:12 +0800

    stopwatch start

--- 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
+++ 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
@@ -1,15 +1,7 @@
-let display = document.getElementById("text");
-display.innerHTML = "JS DOM stopwatch";
-display.style.backgroundColor = "aqua";
-display.style.textAlign = "center";
-display.style.color = "red";
-display.style.border = "solid gray 5px";
-console.log(display);
+let minuts = document.getElementById('min');
+let seconds = document.getElementById('sec');
+let doil = document.getElementById('tens');
 
-let time = document.getElementById("clock");
 
-setInterval(interval, 1000);
-function interval() {
-  time.innerHTML = new Date().getMinutes;
-}
-interval();
+
+
