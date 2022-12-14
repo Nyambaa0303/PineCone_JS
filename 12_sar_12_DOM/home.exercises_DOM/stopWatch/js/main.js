de59459d1557a82064b0d03885e2989e057802ae
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-14 12:38:48 +0800

    random color

--- 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
+++ 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
@@ -1,6 +1,7 @@
 let minuts = document.getElementById('min');
 let seconds = document.getElementById('sec');
 let doil = document.getElementById('tens');
+let clock = document.getElementById('clock')
 
 
 
