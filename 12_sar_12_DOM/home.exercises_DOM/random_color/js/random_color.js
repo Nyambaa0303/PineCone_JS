bf0825749617da58e6457b83e9ad96cf21b3c61d
Evening <evening@admins-iMac.local>
2022-12-14 18:37:18 +0800

    12_sar_14

--- 12_sar_12_DOM/home.exercises_DOM/random_color/js/random_color.js
+++ 12_sar_12_DOM/home.exercises_DOM/random_color/js/random_color.js
@@ -1,11 +1,11 @@
-let button = document.getElementById('btn');
-
-function RandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-    console.log(color)
-  }
+let button = document.getElementById("btn");
+let test = document.getElementById("text");
+let r, g, b;
+function rand() {
+  r = Math.floor(Math.random() * 256);
+  g = Math.floor(Math.random() * 256);
+  b = Math.floor(Math.random() * 256);
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+  test.style.color = "rgb(" + g + ", " + r + ", " + b + ")";
+}
+button.addEventListener("click", rand);
