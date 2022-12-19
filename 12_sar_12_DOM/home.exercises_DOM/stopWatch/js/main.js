2a9d9d0ca3f7cede9964d3ca648d08ae3ce9614b
Evening <evening@admins-iMac.local>
2022-12-19 17:14:00 +0800

    todo list stop watch

--- 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
+++ 12_sar_12_DOM/home.exercises_DOM/stopWatch/js/main.js
@@ -1,8 +1,31 @@
-let minuts = document.getElementById('min');
-let seconds = document.getElementById('sec');
-let doil = document.getElementById('tens');
-let clock = document.getElementById('clock')
+let minutsEl = document.getElementById("min");
+let secondsEl = document.getElementById("sec");
+let doilEl = document.getElementById("tens");
 
+let minut = 00;
+let seconds = 00;
+let doils = 00;
 
+function staartTimer() {
+  doils++;
+  if (doils < 9) {
+    doilEl.innerHTML = "0" + doils;
+  }
 
+  if (doils > 9) {
+    doilEl.innerHTML = doils;
+  }
+  if (doils >= 99) {
+    seconds++;
 
+    secondsEl.innerHTML = "0" + seconds;
+    doils = 0;
+  }
+
+  if (seconds >= 59) {
+    minut++;
+    minutsEl.innerHTML = "0" + minut;
+    seconds = 0;
+  }
+}
+setInterval(staartTimer, 10);
--- 12_sar_14_todo_list/js/todo.js
+++ 12_sar_14_todo_list/js/todo.js
@@ -2,70 +2,74 @@ const taskInputEl = document.getElementById("taskInput");
 
 const buttonEl = document.getElementById("btn");
 const taskListEl = document.getElementById("taskList");
-const deleteButton = document.getElementById('buttonDelete')
-document.getElementById('date').innerHTML = "Он сар өдөр : " + new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + '-' + (new Date().getDate());
+const deleteButton = document.getElementById("buttonDelete");
+document.getElementById("date").innerHTML =
+  "Он сар өдөр : " +
+  new Date().getFullYear() +
+  "-" +
+  (new Date().getMonth() + 1) +
+  "-" +
+  new Date().getDate();
 
 function addTask() {
   addTaskAdd();
 }
 
-  function addTaskAdd() {
-
-    if(taskInputEl.value !== ''){
-
-      //   bvten cardiig aguulj baigaa class buyuu tag
+function addTaskAdd() {
+  if (taskInputEl.value !== "") {
+    //   bvten cardiig aguulj baigaa class buyuu tag
     const newTask = document.createElement("div");
     newTask.className =
       "text-center mt-3 border border-1 rounded border-warning bg-light p-4 d-flex justify-content-between";
-  
+
     // task name ug oroh div
     const newTaskBody = document.createElement("div");
     newTaskBody.className = "text-secondary m-0 p-2";
-  
+
     //   buttonii gaduurhi uusgesen div
     const taskBody2 = document.createElement("div");
     taskBody2.className = "button";
-  
-    //   hamgiin ehnii button uzegnii zuragtai: 
+
+    //   hamgiin ehnii button uzegnii zuragtai:
     const taskBodyBtn = document.createElement("button");
     taskBodyBtn.className = "btn";
-  
+
     //   button tovch dotorhi ocin
     const taskButtonIcon = document.createElement("i");
     taskButtonIcon.className = "bi bi-pen-fill text-secondary";
-  
+
     //  hoyr dahi button
     const taskBodyBtn2 = document.createElement("button");
     taskBodyBtn2.className = "btn";
-  
+
     //   button tovch dotorhi ocin
     const taskButtonIcon2 = document.createElement("i");
     taskButtonIcon2.className = "bi bi-bag-check-fill text-success";
-  
+
     //  gurav dahi button
     const taskBodyBtn3 = document.createElement("button");
     taskBodyBtn3.className = "btn";
-  
+
     //   button tovch dotorhi ocin
     const taskButtonIcon3 = document.createElement("i");
     taskButtonIcon3.className = "bi bi-trash-fill text-danger";
-  
+
     let newTaskBodyText = (document.getElementsByClassName(
       newTaskBody.className
     ).innerHTML = taskInputEl.value);
-  
+
     //   uzegnii zuragtai button
     taskBodyBtn.append(taskButtonIcon);
     taskBody2.append(taskBodyBtn);
-  
+
     // check nogoon button
     taskBodyBtn2.append(taskButtonIcon2);
     taskBody2.append(taskBodyBtn2);
-  
+
     //   trashnii zuragtqi button
     taskBodyBtn3.append(taskButtonIcon3);
     taskBody2.append(taskBodyBtn3);
-  
+
     //   shine uusgesen elemtuud
     newTask.append(newTaskBody);
     //   inputiin valueg div d oruulj ogov
@@ -74,13 +78,8 @@ function addTask() {
     newTask.append(taskBody2);
     taskListEl.append(newTask);
     taskInputEl.value = "";
-    }
-  
   }
-  
-  
-  buttonEl.addEventListener("click", addTask);
- 
-  
+}
 
-
+buttonEl.addEventListener("click", addTask);
+deleteButton.removeEventListener("click", newTask);
