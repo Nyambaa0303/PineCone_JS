cf8d0a73d702cf1730f853b5905e4f662f1b8906
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-22 13:46:13 +0800

    update

---
+++ 12_sar_12_DOM/home.exercises_DOM/click_dropDown/js/main.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('input');
+const select = document.getElementById('colorSelect');
+
+function alert1(){
+    const color = [ulaan, nogoon, tsagaan, har];
+    for(let i = 0; i < color.length; i++){
+        i.value = i.value + i;
+        console.log(i.value)
+    }
+    alert('No. of items : ' + i + '\n' + color[0]);
+}
+
+
+button.addEventListener("click", alert1);
