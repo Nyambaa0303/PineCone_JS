74514c4e550d2c0792b325b3c8cdf5160d5d8200
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-23 12:11:55 +0800

    slide image

---
+++ 12_sar_12_DOM/home.exercises_DOM/image_slider/js/main.js
@@ -0,0 +1,55 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+
+
+function button1(){
+    btn1.style.cssText = "width: 450px; transition:1s";
+    btn2.style.cssText = "width: 120px; transition:1s";
+    btn3.style.cssText = "width: 120px; transition:1s";
+    btn4.style.cssText = "width: 120px; transition:1s";
+    btn5.style.cssText = "width: 120px; transition:1s";
+};
+
+function button2(){
+    btn1.style.cssText = "width: 120px; transition:1s";
+    btn2.style.cssText = "width: 450px; transition:1s";
+    btn3.style.cssText = "width: 120px; transition:1s";
+    btn4.style.cssText = "width: 120px; transition:1s";
+    btn5.style.cssText = "width: 120px; transition:1s";
+};
+
+function button3(){
+    btn1.style.cssText = "width: 120px; transition:1s";
+    btn2.style.cssText = "width: 120px; transition:1s";
+    btn3.style.cssText = "width: 450px; transition:1s";
+    btn4.style.cssText = "width: 120px; transition:1s";
+    btn5.style.cssText = "width: 120px; transition:1s";
+};
+
+function button4(){
+    btn1.style.cssText = "width: 120px; transition:1s";
+    btn2.style.cssText = "width: 120px; transition:1s";
+    btn3.style.cssText = "width: 120px; transition:1s";
+    btn4.style.cssText = "width: 450px; transition:1s";
+    btn5.style.cssText = "width: 120px; transition:1s";
+};
+
+function button5(){
+    btn1.style.cssText = "width: 120px; transition:1s";
+    btn2.style.cssText = "width: 120px; transition:1s";
+    btn3.style.cssText = "width: 120px; transition:1s";
+    btn4.style.cssText = "width: 120px; transition:1s";
+    btn5.style.cssText = "width: 450px; transition:1s";
+};
+
+
+
+
+btn1.addEventListener("click", button1);
+btn2.addEventListener("click", button2);
+btn3.addEventListener("click", button3);
+btn4.addEventListener("click", button4);
+btn5.addEventListener("click", button5);
