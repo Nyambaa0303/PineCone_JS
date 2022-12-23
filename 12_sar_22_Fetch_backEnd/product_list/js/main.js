51d88ce2e09d31db5abe0eb6a6fff0a39012e9ef
Evening <evening@admins-iMac.local>
2022-12-23 18:05:37 +0800

    product list

---
+++ 12_sar_22_Fetch_backEnd/product_list/js/main.js
@@ -0,0 +1,24 @@
+const squaretEl = document.getElementById("square");
+let n = 9;
+fetch("https://dummyjson.com/products")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((products) => {
+    for (let i = 0; i < n; i++) {
+      const div2El = document.createElement("a");
+      div2El.id = `div${i}`;
+      const cards = `<div class = "card">
+        <img src = "${products.products[i].thumbnail}" alt = ""/>
+        <h3 class = "title">${products.products[i].title}</h3>
+        <p>${products.products[i].description}</p>
+        <p>${products.products[i].brand}</p>
+        <p class = "price">${products.products[i].price}<b>$<b/></p>
+        </div>`;
+      cards.id = `div${i}`;
+      div2El.innerHTML = div2El.innerHTML + cards;
+      squaretEl.append(div2El);
+    }
+  });
