d6b226b3e24c0f92f1d3e0752da08514c0736581
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-26 23:51:47 +0800

    userProfile

--- 12_sar_22_Fetch_backEnd/UserProfile/js/main.js
+++ 12_sar_22_Fetch_backEnd/UserProfile/js/main.js
@@ -1,4 +1,4 @@
-let n = 10;
+let n = 1;
 let profileEl = document.getElementById("profile");
 
 fetch("https://randomuser.me/api")
@@ -14,14 +14,17 @@ fetch("https://randomuser.me/api")
       const cards = `<div class = "card card${i}">
         <div class = "call"> 
         <img src="${api.results[i].picture.thumbnail}" alt="pp">
-        <h3 class="title">${api.results[i].name.title} : ${api.results[i].name.first}  ${api.results[i].name.last}</h3>
+        <h2 class="title">${api.results[i].name.title} : ${api.results[i].name.first}  ${api.results[i].name.last}</h2>
         <p class = "phone">Phone: ${api.results[i].phone}</p>
         <p class = "mail">Email: ${api.results[i].email}</p>
         </div>
         <div class = "bottom">
-        <p class = "gender">Gender: ${api.results[i].gender}</p>
-        <p class = "age">Age: ${api.results[i].dob.age}</p>
+        <p class = "gender"><span class="span"></span>Gender: ${api.results[i].gender}</p>
+        <p class = "age"><span class="span1"></span>Age: ${api.results[i].dob.age}</p>
+        <p><span class="span2"></span>Date: ${api.results[i].registered.date}</p>
+        
         </div>
+        <div class = "spend">Total Amount Spent $ 2,314</div>
         </div>`;
       proEl.innerHTML = proEl.innerHTML + cards;
       profileEl.append(proEl);
