49af45cf01c93f18b5221215b253453eb241c475
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2022-12-27 13:31:11 +0800

    userProfile

--- 12_sar_22_Fetch_backEnd/UserProfile/js/main.js
+++ 12_sar_22_Fetch_backEnd/UserProfile/js/main.js
@@ -1,4 +1,4 @@
-let n = 1;
+
 let profileEl = document.getElementById("profile");
 
 fetch("https://randomuser.me/api")
@@ -8,7 +8,7 @@ fetch("https://randomuser.me/api")
     }
   })
   .then((api) => {
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 1; i++) {
       const proEl = document.createElement("div");
       proEl.className = `pro pro${i}`;
       const cards = `<div class = "card card${i}">
@@ -28,5 +28,73 @@ fetch("https://randomuser.me/api")
         </div>`;
       proEl.innerHTML = proEl.innerHTML + cards;
       profileEl.append(proEl);
+    }
+  });
+
+  // ariin huudasnii js code
+
+  let leftEl = document.getElementById("left");
+
+fetch("https://randomuser.me/api")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((data) => {
+    for (let i = 0; i < 1; i++) {
+      const pro1El = document.createElement("div");
+      pro1El.className = `pro1 pro1${i}`;
+      const cards = `<div class = "card card${i}">
+        <div class = "call"> 
+        <img src="${data.results[i].picture.thumbnail}" alt="pp">
+        <h2 class="title">${data.results[i].name.title} : ${data.results[i].name.first}  ${data.results[i].name.last}</h2>
+        <p class = "phone">Phone: ${data.results[i].phone}</p>
+        <p class = "mail">Email: ${data.results[i].email}</p>
+        </div>
+        <div class = "bottom">
+        <p class = "gender"><span class="span"></span>Gender: ${data.results[i].gender}</p>
+        <p class = "age"><span class="span1"></span>Age: ${data.results[i].dob.age}</p>
+        <p><span class="span2"></span>Date: ${data.results[i].registered.date}</p>
+        
+        </div>
+        <div class = "spend">Total Amount Spent $ 2,314</div>
+        </div>`;
+      pro1El.innerHTML = pro1El.innerHTML + cards;
+      leftEl.append(pro1El);
+    }
+  });
+
+  // 3 dahi card 
+
+  let rigthEl = document.getElementById("right");
+
+fetch("https://randomuser.me/api")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((rigth) => {
+    for (let i = 0; i < 1; i++) {
+      const pro2El = document.createElement("div");
+      pro2El.className = `pro2 pro2${i}`;
+      const cards = `<div class = "card card${i}">
+        <div class = "call"> 
+        <img src="${rigth.results[i].picture.thumbnail}" alt="pp">
+        <h2 class="title">${rigth.results[i].name.title} : ${rigth.results[i].name.first}  ${rigth.results[i].name.last}</h2>
+        <p class = "phone">Phone: ${rigth.results[i].phone}</p>
+        <p class = "mail">Email: ${rigth.results[i].email}</p>
+        </div>
+        <div class = "bottom">
+        <p class = "gender"><span class="span"></span>Gender: ${rigth.results[i].gender}</p>
+        <p class = "age"><span class="span1"></span>Age: ${rigth.results[i].dob.age}</p>
+        <p><span class="span2"></span>Date: ${rigth.results[i].registered.date}</p>
+        
+        </div>
+        <div class = "spend">Total Amount Spent $ 2,314</div>
+        </div>`;
+      pro2El.innerHTML = pro2El.innerHTML + cards;
+      rigthEl.append(pro2El);
     }
   });
