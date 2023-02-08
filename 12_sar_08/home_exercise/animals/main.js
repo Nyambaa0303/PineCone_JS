cb5a6eb5f50ef33d0dc947138c98863cc2afb49a
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-02-08 12:02:47 +0800

    objet

---
+++ 12_sar_08/home_exercise/animals/main.js
@@ -0,0 +1,7 @@
+let animals = [
+  { type: "dog", sound: "woof, woof" },
+  { type: "cow", sound: "moo, moo" },
+  { type: "cat", sound: "meow, meow" },
+  { type: "fox", sound: "frog, frog" },
+  { type: "duck", sound: "quack, quack" },
+];
